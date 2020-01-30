84b9cc46bcda1c192558012925f6c74e642ddb58
Rodolfo Aranguiz <rodolfoap260@gmail.com>
2020-01-29 23:00:16 -0300

    Pruebas de conexion sql rodolfo

--- app/Http/Controllers/pago_facil/HomeController.php
+++ app/Http/Controllers/pago_facil/HomeController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers\pago_facil;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\UsersKio_Model;
 
 class HomeController extends Controller
 {
     public function index(){
-        return view('pago_facil.home.index', compact(''));
+        
+        echo UsersKio_Model::count(); 
+        // return view('pago_facil.home.index', compact(''));
     }
 }
