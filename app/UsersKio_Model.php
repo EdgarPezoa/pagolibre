cca70347bfebf6b95fac9619f6da00cbe32e1cd0
Rodolfo Aranguiz <rodolfoap260@gmail.com>
2020-02-01 18:42:40 -0300

    userskio_model: modificación de fecha pruebas

--- app/UsersKio_Model.php
+++ app/UsersKio_Model.php
@@ -9,4 +9,5 @@ class UsersKio_Model extends Model
     public $timestamps = false;
     protected $table = 'tbl_SystemUser';    
     protected $primaryKey = 'fld_id';
+    protected $dateFormat = 'Y-d-m H:i:s';
 }
