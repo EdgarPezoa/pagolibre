b48db857469f3d7e5c59e65abf559039e0f1ea34
Enzo Eduardo <enzoflores05@gmail.com>
2020-02-01 23:07:44 -0300

    Login exitoso

--- app/Http/Controllers/pago_facil/HomeController.php
+++ app/Http/Controllers/pago_facil/HomeController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers\pago_facil;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\UsersKio_Model;
+use Hash;
 
 class HomeController extends Controller
 {
     public function index(){
 
+        // dd(Hash::make('eddy'));
         dd(UsersKio_Model::all());
         return view('pago_facil.home.index', compact(''));
     }
--- app/UsersKio_Model.php
+++ app/UsersKio_Model.php
@@ -3,9 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Auth\Authenticatable;
 
-class UsersKio_Model extends Model
+class UsersKio_Model extends Model implements AuthenticatableContract
 {
+    use Authenticatable;
+    
     public $timestamps = false;
     protected $table = 'tbl_SystemUser';    
     protected $primaryKey = 'fld_id';
