1ddcfcbbf29a844dadebeb08e6755cca3be100dd
Edgar Pezoa <pezoa.1@gmail.com>
2020-02-19 13:31:25 -0300

    pruebas flow

--- app/Http/Controllers/Flow/FlowController.php
+++ app/Http/Controllers/Flow/FlowController.php
@@ -137,7 +137,26 @@ class FlowController extends Controller
     }
 
     public function confirm(Request $request){
-        dd($request);
+        try {
+            if(!isset($_POST["token"])) {
+                throw new Exception("No se recibio el token", 1);
+            }
+            $token = filter_input(INPUT_POST, 'token');
+            $params = array(
+                "token" => $token
+            );
+            $serviceName = Utils::PAYMENT_CREATE_GET_STATUS;
+            $this->sendRequest();
+            $response = $flowApi->send($serviceName, $params, "GET");
+            
+            //Actualiza los datos en su sistema
+            
+            dd($response);
+            
+            
+        } catch (Exception $e) {
+            echo "Error: " . $e->getCode() . " - " . $e->getMessage();
+        }
     }
 
     public function prueba(){
