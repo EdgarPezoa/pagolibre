d18ed681c741f58107417485cf950ea30d105e0a
Edgar Pezoa <pezoa.1@gmail.com>
2020-02-19 15:56:24 -0300

    pruebas flow

---
+++ app/Models/InvoiceModel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class InvoiceModel extends Model
+{
+    protected $connection = 'KlmIOT_sqlsrv';
+    protected $table = 'tbl_Em_Invoice';
+    protected $primaryKey = 'fld_Id';
+    public $timestamps = true;
+
+    protected $fillable = [
+        '',
+    ];
+
+    // DB::connection('KlmIOT_sqlsrv')->select("select i.fld_Id, i.fld_DeviceDescription, i.fld_InvoiceStartDateTime, i.fld_InvoiceEndDateTime, i.fld_InvoiceStartIndex, i.fld_InvoiceEndIndex, i.fld_InvoiceConsumption, i.fld_InvoiceAmount from tbl_EM_Invoice i inner join tbl_EM_InvoiceDetail d on i.fld_Id=d.fld_InvoiceId inner join tbl_EM_Device de on d.fld_DeviceId=de.fld_Id where de.fld_Id=4 and i.fld_InvoiceConfirmed = 1;");
+}
