a7388718b856867c7f54ec56161eada318d833de
Edgar Pezoa <pezoa.1@gmail.com>
2020-02-19 17:44:09 -0300

    base de datos

--- resources/views/layouts/pago_libre/backend/menu.blade.php
+++ resources/views/layouts/pago_libre/backend/menu.blade.php
@@ -14,7 +14,7 @@
                 <ul class="nav-main">
                     <li>
                         <a class="text-black {{ Request::is('intranet') ? 'active' : '' }}" href="{{ route('pagolibre_index') }}"><i class="si si-speedometer"></i><span class="sidebar-mini-hide">Escritorio</span></a>
-                        <a class="text-black {{ Request::is('intranet/historicos') ? 'active' : '' }}" href="{{ route('pagolibre_historicos') }}"><i class="si si-speedometer"></i><span class="sidebar-mini-hide">Historico</span></a>
+                        <a class="text-black {{ Request::is('intranet/historicos') ? 'active' : '' }}" href="{{ route('pagolibre_historicos') }}"><i class="fa fa-calendar"></i><span class="sidebar-mini-hide">Historico</span></a>
                     </li>                                                                                                                                                   
                 </ul>
             </div>            
