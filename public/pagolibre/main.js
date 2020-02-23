df616ff8f931ff3d81f91fff8de0884a4d98be2b
Rodolfo Aranguiz <rodolfoap260@gmail.com>
2020-02-23 02:59:42 -0300

    fade out

---
+++ public/pagolibre/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    
+    $('.closeBtn').click(function (e) {         
+        $(this).parents('.row').fadeOut(400);
+    });
+    
+});
--- resources/views/pago_libre/home/index.blade.php
+++ resources/views/pago_libre/home/index.blade.php
@@ -15,7 +15,7 @@
 <div class="row">
     <div class="col-lg-12 col-md-12 col-sm-12">
     <div class="alert alert-success alert-dismissible" role="alert">
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <button type="button" class="close closeBtn"><span>&times;</span></button>
         <strong>Éxito: </strong>{{Session::get('success')}}
     </div>
     </div>
@@ -25,7 +25,7 @@
 <div class="row">
     <div class="col-lg-12 col-md-12 col-sm-12">
     <div class="alert alert-danger alert-dismissible" role="alert">
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <button type="button" class="close closeBtn" ><span>&times;</span></button>
         <strong>Error: </strong>{{Session::get('error')}}
     </div>
     </div>
