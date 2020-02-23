c692d6c2bb12ed4b4a86c0731cac37db32f30116
Edgar Pezoa <pezoa.1@gmail.com>
2020-02-22 23:55:26 -0300

    Modelo transaccion string

--- app/Models/TransaccionModel.php
+++ app/Models/TransaccionModel.php
@@ -14,14 +14,19 @@ class TransaccionModel extends Model
     protected $fillable = [
         'cod_usuario',
         'invoice_id',
+        'cod_estado',
+        'flowOrder',
         'subject',
         'amount',
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
+        'paymentMedia',
+        'payerEmail',
+        'paymenteFee',
+        'paymenteTaxes',
+        'paymenteBalance',
+        'requestDate',
+        'paymentDate',
+        'paymentTransferDate',
+        'updated_at',
+        'created_at'
     ];
 }
