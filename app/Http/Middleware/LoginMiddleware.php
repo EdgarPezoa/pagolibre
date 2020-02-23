949463b54d9833c002b27e4b6bf2b3a9b0aee002
Rodolfo Aranguiz <rodolfoap260@gmail.com>
2020-02-23 00:36:12 -0300

    xd

--- app/Http/Middleware/LoginMiddleware.php
+++ app/Http/Middleware/LoginMiddleware.php
@@ -16,11 +16,11 @@ class LoginMiddleware
      */
     public function handle($request, Closure $next)
     {
-        if (Auth::check()) {
-            return $next($request);
-        }else{
-            return redirect()->route('login');
-        }
-        // return $next($request);
+        // if (Auth::check()) {
+        //     return $next($request);
+        // }else{
+        //     return redirect()->route('login');
+        // }
+        return $next($request);
     }
 }
