c7d4f17babf193a4adb15fbf13136dd7a22e1d72
Rodolfo Aranguiz <rodolfoap260@gmail.com>
2020-02-23 02:36:40 -0300

    fade out

--- resources/views/pago_libre/home/index.blade.php
+++ resources/views/pago_libre/home/index.blade.php
@@ -12,7 +12,7 @@
 
 @section('content')
 @if(Session::has('success'))
-<div class="row animated fadeOut">
+<div class="row">
     <div class="col-lg-12 col-md-12 col-sm-12">
     <div class="alert alert-success alert-dismissible" role="alert">
         <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
