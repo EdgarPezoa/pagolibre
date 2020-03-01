f33b04df92d6e67eecbc49787a32ae756e7db511
Rodolfo Aranguiz <rodolfoap260@gmail.com>
2020-03-01 02:47:32 -0300

    server xD

--- resources/views/pago_libre/transaccion/result.blade.php
+++ resources/views/pago_libre/transaccion/result.blade.php
@@ -1,49 +1,45 @@
 @extends('layouts.pago_libre.backend.master')
 @section('title', 'Dashboard')
 @section('pageHeader')
-
+<div class="content bg-gray-lighter">
+    <div class="row items-push">
+        <div class="col-sm-12">
+            <h1 class="page-heading">
+                Pago Finalizado
+            </h1>
+        </div>        
+    </div>
+</div>
 @endsection
 
 @section('content')
-<div class="content content-boxed">
-    <!-- Demo Content -->
-    <div class="block block-rounded">
-        <div class="block-header">            
-            <h1 class="block-title">PAGO FINALIZADO</h1>
 
-        </div>
-        <h3>Información de Pago </h3><hr>
-        <div class="block-content block-content-full block-content-narrow">
-            <div class="push-30 push-30-t">
-
-                <table class="table">                    
-                    <tbody>                        
-                        <tr>
-                            <td>Orden Flow</td><td>{{ $response['flowOrder'] }}</td>
-                        </tr>
-                        <tr>
-                            <td>Orden de comercio</td><td>{{ $response['commerceOrder'] }}</td>
-                        </tr>
-                        <tr>
-                            <td>Descripción</td><td>{{ $response['subject'] }}</td>
-                        </tr>
-                        <tr>
-                            <td>Monto</td><td>{{ $response['amount'] }}</td>
-                        </tr>
-                        <tr>
-                            <td>Correo del Pagador</td><td>{{ $response['payer'] }}</td>                                                        
-                        </tr>                          
-                        <tr>
-                            <td>Fecha de Pago</td><td>{{  $response['paymentData']['date'] }}</td>
-                        </tr>              
-                        <tr>
-                            <td>Método de Pago</td><td>{{  $response['paymentData']['media'] }}</td>
-                        </tr>              
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
-    <!-- END Demo Content -->
+<div class="content">
+    <h2 class="content-heading">Your content</h2>
+    <table class="table">                    
+        <tbody>                        
+            <tr>
+                <td>Orden Flow</td><td>{{ $response['flowOrder'] }}</td>
+            </tr>
+            <tr>
+                <td>Orden de comercio</td><td>{{ $response['commerceOrder'] }}</td>
+            </tr>
+            <tr>
+                <td>Descripción</td><td>{{ $response['subject'] }}</td>
+            </tr>
+            <tr>
+                <td>Monto</td><td>{{ $response['amount'] }}</td>
+            </tr>
+            <tr>
+                <td>Correo del Pagador</td><td>{{ $response['payer'] }}</td>                                                        
+            </tr>                          
+            <tr>
+                <td>Fecha de Pago</td><td>{{  $response['paymentData']['date'] }}</td>
+            </tr>              
+            <tr>
+                <td>Método de Pago</td><td>{{  $response['paymentData']['media'] }}</td>
+            </tr>              
+        </tbody>
+    </table>    
 </div>
 @endsection
