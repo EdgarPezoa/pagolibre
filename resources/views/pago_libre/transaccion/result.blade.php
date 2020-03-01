954490f36c39f7e9336df1c6b2ad5c8314c54447
Rodolfo Aranguiz <rodolfoap260@gmail.com>
2020-03-01 02:30:09 -0300

    server xD

--- resources/views/pago_libre/transaccion/result.blade.php
+++ resources/views/pago_libre/transaccion/result.blade.php
@@ -1,24 +1,27 @@
 @extends('layouts.pago_libre.backend.master')
 @section('title', 'Dashboard')
 @section('pageHeader')
-<div class="content bg-image overflow-hidden" style="background-image: url('{{ asset('img/377.jpg') }}'); min-height:250px; position:relative;">
-    <div class="header-custom animated zoomIn text-right">
-        <h2>Con pago libre has ahorrado:$89.520</h2>
-        <p>Total pagado en PagoLibre ultimos 12 meses: $298.400</p>
-        <p>Valor cobrado por distribuidora en los ultimos 12 meses: $387.920</p>
-    </div>
-</div>
+
 @endsection
 
 @section('content')
-<h2 class="content-heading">Paga tus cuentas rapido y Seguro !</h2>
-<div class="row">
-    <div class="col-md-12">
-        <div class="block">
-        
-        {{ print_r($response) }}
-
+<div class="content content-boxed">
+    <!-- Demo Content -->
+    <div class="block block-rounded">
+        <div class="block-header">
+            <ul class="block-options">
+                <li>
+                    <button type="button"><i class="si si-settings"></i></button>
+                </li>
+            </ul>
+            <h3 class="block-title">Demo Content</h3>
+        </div>
+        <div class="block-content block-content-full block-content-narrow">
+            <div class="push-30 push-30-t">
+                {{ print_r($response) }}
+            </div>
         </div>
     </div>
+    <!-- END Demo Content -->
 </div>
 @endsection
