3773eabfb28f79d2a54a5f6b73982c2f28408ea4
Rodolfo Aranguiz <rodolfoap260@gmail.com>
2020-03-01 03:25:58 -0300

    server xD

--- resources/views/pago_libre/transaccion/result.blade.php
+++ resources/views/pago_libre/transaccion/result.blade.php
@@ -16,7 +16,7 @@
 <div class="content">
     <h2 class="content-heading">Información</h2>
     <div class="col-md-6">
-        <table class="table table-striped table-sm">                    
+        <table class="table table-striped table-borderless table-header-bg">                    
             <tbody> 
                 <thead class="thead-dark">
                     <tr><th>Dato</th><th></th></tr>
