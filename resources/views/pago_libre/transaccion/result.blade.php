b554203e33fab8d18f6b7f3684cccad655ac1657
Rodolfo Aranguiz <rodolfoap260@gmail.com>
2020-03-01 03:02:32 -0300

    server xD

--- resources/views/pago_libre/transaccion/result.blade.php
+++ resources/views/pago_libre/transaccion/result.blade.php
@@ -15,33 +15,38 @@
 @section('content')
 
 <div class="content">
-    <h2 class="content-heading">INFORMACIÓN DE PAGO</h2>
-    <table class="table">                    
-        <tbody>                        
-            <tr>
-                <td>Orden Flow</td><td>{{ $response['flowOrder'] }}</td>
-            </tr>
-            <tr>
-                <td>Orden de comercio</td><td>{{ $response['commerceOrder'] }}</td>
-            </tr>
-            <tr>
-                <td>Descripción</td><td>{{ $response['subject'] }}</td>
-            </tr>
-            <tr>
-                <td>Monto</td><td>{{ $response['amount'] }}</td>
-            </tr>
-            <tr>
-                <td>Correo del Pagador</td><td>{{ $response['payer'] }}</td>                                                        
-            </tr>                          
-            @if($response['paymentData'])
-            <tr>
-                <td>Fecha de Pago</td><td>{{  $response['paymentData']['date'] }}</td>
-            </tr>              
-            <tr>
-                <td>Método de Pago</td><td>{{  $response['paymentData']['media'] }}</td>
-            </tr>             
-            @endif
-        </tbody>
-    </table>    
+    <h2 class="content-heading">Comprobante de pago</h2>
+    <div class="col-md-6">
+        <table class="table">                    
+            <tbody>                        
+                <tr>
+                    <td>Orden Flow</td><td>{{ $response['flowOrder'] }}</td>
+                </tr>
+                <tr>
+                    <td>Orden de comercio</td><td>{{ $response['commerceOrder'] }}</td>
+                </tr>
+                <tr>
+                    <td>Descripción</td><td>{{ $response['subject'] }}</td>
+                </tr>
+                <tr>
+                    <td>Monto</td><td>{{ $response['amount'] }}</td>
+                </tr>
+                <tr>
+                    <td>Correo del Pagador</td><td>{{ $response['payer'] }}</td>                                                        
+                </tr>                          
+                @if($response['paymentData'])
+                <tr>
+                    <td>Fecha de Pago</td><td>{{  $response['paymentData']['date'] }}</td>
+                </tr>              
+                <tr>
+                    <td>Método de Pago</td><td>{{  $response['paymentData']['media'] }}</td>
+                </tr>             
+                @endif
+            </tbody>
+        </table>    
+    </div>
+    <div class="col-md-6">
+        <img src="" alt="">
+    </div>
 </div>
 @endsection
