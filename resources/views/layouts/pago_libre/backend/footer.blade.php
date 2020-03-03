2969122226873fd096751a7bb9d0ffd72002c636
Edgar Pezoa <pezoa.1@gmail.com>
2020-03-03 14:08:38 -0300

    link a pagian

--- resources/views/layouts/pago_libre/backend/footer.blade.php
+++ resources/views/layouts/pago_libre/backend/footer.blade.php
@@ -1,6 +1,6 @@
 <footer id="page-footer" class="content-mini content-mini-full font-s12 bg-gray-lighter clearfix">
     <div class="pull-right">
-        Creado con <i class="fa fa-heart text-city"></i> por <a class="font-w600" href="www.edgarpezoa.cl" target="_blank">Edgar Pezoa</a>
+        Creado con <i class="fa fa-heart text-city"></i> por <a class="font-w600" href="https://www.edgarpezoa.cl/" target="_blank">Edgar Pezoa</a>
     </div>
     <div class="pull-left">
         <a class="font-w600" href="javascript:void(0)" target="_blank">Pago Libre</a> &copy; 2020
